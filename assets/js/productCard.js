9d7d507aa3da9b361b7b9bdcdbedefd2e8e0b1cb
chube0dan <dinhnghiamt123@gmail.com>
2024-03-17 18:48:53 +0700

    upload source code

---
+++ assets/js/productCard.js
@@ -0,0 +1,28 @@
+function productCard({id, title, price, image, image2, category}) {
+    return `
+        <div class="swiper-slide product-card" data-id="${id}" data-category="${category}" data-image1="${image}" data-image2="${image2}">
+            <div class="product-card__image">
+                <img src="${image}" alt="${title}"/>
+            </div>
+            <div class="product-card__description">
+                <div class="row">
+                    <div class="product-card__title">${title}</div>
+                    <button class="product-card__btn btn">
+                        <i class="fa-regular fa-heart"></i>
+                    </button>
+                </div>
+                <div class="row">
+                    <div class="product-card__price">${price}</div>
+                    <button class="explore-more btn">
+                    Explore more <i class="fa-solid fa-arrow-right-long"></i>
+                    </button>
+                </div>
+                <span class="border-animation"></span>
+                <span class="border-animation"></span>
+                <span class="border-animation"></span>
+            </div>
+        </div>
+        `;
+}
+
+export default productCard;
